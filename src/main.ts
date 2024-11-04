9221fa1b4519f4b0bb74a7d160fcf0353a015396
MrSak1996 <kimsacluti10101996@gmail.com>
2024-11-04 09:18:30 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import './assets/css/satoshi.css'
+import './assets/css/style.css'
+import 'jsvectormap/dist/css/jsvectormap.min.css'
+import 'flatpickr/dist/flatpickr.min.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import VueApexCharts from 'vue3-apexcharts'
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+
+app.use(router)
+app.use(VueApexCharts)
+
+app.mount('#app')
